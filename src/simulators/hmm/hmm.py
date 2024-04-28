57b3e831dfd85f4d4d75a3a4ff199c7cf3b1b633
Rafael Zimmer <rzimmerdev@gmail.com>
2024-04-28 17:13:05 -0300

    # TODO: HMM simulator async def_run callback

--- src/simulators/hmm/hmm.py
+++ src/simulators/hmm/hmm.py
@@ -2,40 +2,11 @@ import numpy as np
 from scipy.stats import norm
 from plotly import graph_objects as go
 
-
-def mse(x, y):
-    return np.mean((x - y) ** 2)
-
-
-def mixture_sampling_normal(transition_matrix, emission_matrix, space, T):
-    current_state = 0
-    observations = []
-
-    for t in range(T):
-        current_state = np.random.choice(np.arange(len(transition_matrix)), p=transition_matrix[current_state])
-        observation = np.random.choice(space, p=emission_matrix[current_state])
-
-        observations.append(observation)
-
-    return np.array(observations)
-
-
-def mixture_sampling_any(transition_matrix, emission_distribution, space, T):
-    current_state = 0
-    observations = []
-
-    for t in range(T):
-        current_state = np.random.choice(np.arange(len(transition_matrix)), p=transition_matrix[current_state])
-        histogram = emission_distribution[current_state]
-        observation = np.random.choice(space, p=histogram)
-
-        observations.append(observation)
-
-    return np.array(observations)
+from .utils import mse, mixture_sampling_normal, mixture_sampling_any
 
 
 class HMM:
-    def __init__(self, states, space, d):
+    def __init__(self, states: int, space: np.ndarray, d: int):
         self.hidden_states = states
         self.observation_space = space
         self.d = d
--- src/simulators/hmm/hmm_simulator.py
+++ src/simulators/hmm/hmm_simulator.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from .hmm import HMM
 from ...lob import LOBSimulator, EventSampler, ArrivalSampler
 
@@ -6,8 +8,10 @@ class HMMArrivalSampler(ArrivalSampler):
     def __init__(self, hmm: HMM, *args, **kwargs):
         self.hmm = hmm
 
-    def __call__(self, *args, **kwargs):
-        pass
+    def __call__(self, state, *args, **kwargs) -> float:
+        state = self.hmm.observation_index(state)
+        observation = np.random.choice(self.hmm.observation_space, p=self.hmm.emission_matrix[state])
+        return observation
 
 
 class HMMEventSampler(EventSampler):
@@ -16,5 +20,27 @@ class HMMEventSampler(EventSampler):
 
 
 class HMMSimulator(LOBSimulator):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, hmm, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.hmm = hmm
+        self.state = np.random.choice(np.arange(self.hmm.hidden_states))
+        self.arrival_sampler = HMMArrivalSampler(self.hmm)
+        self.event_sampler = HMMEventSampler()
+
+    def simulate(self):
+        pass
+
+
+def main():
+    # using dash to plot realtime event times:
+    import dash
+    from dash import dcc
+
+    # create a HMM object
+    hmm = HMM(2, np.linspace(0, 1, 100), 1)
+
+    # create a HMM simulator
+    hmm_simulator = HMMSimulator(hmm, 1000, 1000)
+
+    # run the simulation
+
