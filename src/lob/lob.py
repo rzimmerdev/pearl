c80846d9d1c8be3ec3b0980761e8c25af1fed88a
Rafael Zimmer <rzimmerdev@gmail.com>
2024-04-28 16:47:57 -0300

    Working simulator with event and time sampler

---
+++ src/lob/lob.py
@@ -0,0 +1,88 @@
+import dxlib as dx
+import plotly.graph_objects as go
+
+
+class LOB:
+    def __init__(self):
+        self.bids = []
+        self.asks = []
+
+    def send(self, order: dx.Signal, order_type: dx.OrderType = dx.OrderType.LIMIT):
+        if order_type == dx.OrderType.MARKET:
+            self.send_market(order)
+        if order.side == dx.Side.BUY:
+            self.bids.append(order)
+        elif order.side == dx.Side.SELL:
+            self.asks.append(order)
+
+    def aggregate(self):
+        # make orders with the same price into one
+        for i in range(len(self.bids)):
+            for j in range(i+1, len(self.bids)):
+                if self.bids[i].price == self.bids[j].price:
+                    self.bids[i].quantity += self.bids[j].quantity
+                    self.bids.pop(j)
+
+    def send_market(self, order: dx.Signal):
+        if order.side == dx.Side.BUY:
+            while order.quantity > 0:
+                ask = self.asks[0]
+                if ask.quantity > order.quantity:
+                    ask.quantity -= order.quantity
+                    order.quantity = 0
+                else:
+                    order.quantity -= ask.quantity
+                    self.asks.pop(0)
+        else:
+            while order.quantity > 0:
+                bid = self.bids[0]
+                if bid.quantity > order.quantity:
+                    bid.quantity -= order.quantity
+                    order.quantity = 0
+                else:
+                    order.quantity -= bid.quantity
+                    self.bids.pop(0)
+
+    def plot(self, depth: int = None):
+        depth_label = f'{depth} levels'
+        if depth is None:
+            depth = len(self.asks)
+            depth_label = "Full"
+        ask_prices = [x.price for x in self.asks][:depth]
+        ask_volumes = [x.quantity for x in self.asks][:depth]
+
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+            y=['{:.4f}'.format(x) for x in ask_prices],
+            x=ask_volumes,
+            orientation='h',
+            marker=dict(
+                color='Gray',
+            ),
+            name="Asks"
+        ))
+        bid_prices = [x.price for x in self.bids]
+        bid_volumes = [x.quantity for x in self.bids]
+        fig.add_trace(go.Bar(
+            y=['{:.4f}'.format(x) for x in bid_prices],
+            x=bid_volumes,
+            orientation='h',
+            marker=dict(
+                color='IndianRed',
+            ),
+            name="Bids"
+        ))
+        fig.update_layout(barmode='stack',
+                          xaxis_title="Volume",
+                          yaxis_title="Price",
+                          title="Limit Order Book, depth={}".format(depth_label))
+        fig.show()
+
+
+if __name__ == "__main__":
+    lob = LOB()
+    lob.send(dx.Signal(dx.Side.BUY, 10, 100))
+    lob.send(dx.Signal(dx.Side.BUY, 10, 101))
+    lob.send(dx.Signal(dx.Side.SELL, 10, 102))
+    lob.send(dx.Signal(dx.Side.SELL, 10, 101), dx.OrderType.MARKET)
+    lob.plot()
---
+++ src/lob/simulator.py
@@ -0,0 +1,74 @@
+from abc import ABC
+from typing import List, Tuple
+
+import dxlib as dx
+import asyncio
+
+from .lob import LOB
+
+
+class EventSampler(ABC):
+    def __call__(self, *args, **kwargs) -> dx.Signal:
+        pass
+
+
+class ArrivalSampler(ABC):
+    def __call__(self, *args, **kwargs) -> float:
+        pass
+
+
+class TestEventSampler(EventSampler):
+    def __call__(self, *args, **kwargs) -> dx.Signal:
+        return dx.Signal(
+            side=dx.Side.BUY,
+            price=1.0,
+            quantity=1.0,
+        )
+
+
+class TestArrivalSampler(ArrivalSampler):
+    def __call__(self, *args, **kwargs) -> float:
+        return 1.0
+
+
+class LOBSimulator:
+    def __init__(self, event_sampler: EventSampler, arrival_sampler: callable, logger: dx.LoggerMixin = None):
+        self.lob = LOB()
+        self.t = 0
+        self.logger = logger or dx.InfoLogger()
+
+        self.event_sampler = event_sampler
+        self.arrival_sampler = arrival_sampler
+        self.events: List[Tuple[float, dx.Signal]] = []
+
+    async def _step(self):
+        try:
+            # sample arrival time and wait
+            arrival_time = self.arrival_sampler()
+            await asyncio.sleep(arrival_time)
+
+            # sample event and register
+            event = self.event_sampler()
+            t = self.register(event, arrival_time)
+
+            self.logger.info(f"Event {event} registered at time {t}")
+            return t, event
+        except asyncio.CancelledError:
+            return None
+
+    def register(self, event: dx.Signal, time: float):
+        self.t += time
+        self.events.append((time, event))
+        self.lob.send(event)
+        self.lob.aggregate()
+        return self.t
+
+    async def _run(self, T: float = None):
+        while self.t < T:
+            await self._step()
+
+    def run(self, T: float = 10.0):
+        self.logger.info("Starting simulation")
+        asyncio.run(self._run(T))
+        self.logger.info("Simulation finished")
+        return self.lob
